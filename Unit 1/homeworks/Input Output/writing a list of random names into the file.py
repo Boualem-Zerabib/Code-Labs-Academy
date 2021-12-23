c81dbd10689bcb348cdc7b382fd1942fce3241d1
Boualem-Zerabib <96547903+Boualem-Zerabib@users.noreply.github.com>
2021-12-23 22:06:32 +0100

    Update writing a list of random names into the file.py

--- Unit 1/homeworks/Input Output/writing a list of random names into the file.py
+++ Unit 1/homeworks/Input Output/writing a list of random names into the file.py
@@ -3,6 +3,6 @@ students = file.read()
 file = open('./Input Output/Student_names.txt','w')
 new_students = ["\nJeffery Garza", "\nAarron Markham", "\nKunal Lambert", "\nMusa Copeland", "\nKaylan Preston", "\nAntonia Quintero", "\nKacie Davey"]
 for k in new_students:
-    file.write(students)
+    file.write(new_students)
 print("the new list of student names after adding some random ones is:", students)
 
